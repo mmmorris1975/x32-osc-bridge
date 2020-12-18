0ab382111781075ce151298efcb716a4116285b2
Mike Morris <michael.m.morris@gmail.com>
2020-12-18 12:25:48 -0600

    remove duplicate workspace file update

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -2,21 +2,19 @@ set -e -x
 set -o pipefail
 
 NAME="x32-osc-bridge"
+VER=$(git describe --tags)
 
 function build() {
-  VER=$(git describe --tags)
   BUILDDIR=${1:-"."}/${GOOS}-${GOARCH}
-
   mkdir -p $BUILDDIR
   go build -ldflags "-s -w -X main.Version=${VER}" -o $BUILDDIR/
-  echo $VER >${BUILDDIR}/../version
 }
 
 function pkg_zip() {
   cd $1
   upx *
   chmod +x *
-  zip ${NAME}_$(cat ../version)_$(basename $PWD | tr '-' '_').zip *
+  zip ${NAME}_${VER}_$(basename $PWD | tr '-' '_').zip *
   mv *.zip ../artifacts/
   cd ..
 }
@@ -32,7 +30,7 @@ function pkg_rpm() {
   fi
 
   cd $1
-  fpm --verbose -s dir -t rpm --name $NAME --version $(cat ../version) --license MIT --architecture $RPM_ARCH \
+  fpm --verbose -s dir -t rpm --name $NAME --version $VER --license MIT --architecture $RPM_ARCH \
     --provides $NAME --description $NAME --url "https://github.com/mmmorris1975/$NAME" --maintainer 'mmmorris1975@github' \
     --rpm-user bin --rpm-group bin --rpm-digest sha1 --prefix /usr/local/bin *
   mv *.rpm ../artifacts/
@@ -50,7 +48,7 @@ function pkg_deb() {
   fi
 
   cd $1
-  fpm --verbose -s dir -t deb --name $NAME --version $(cat ../version) --license MIT --architecture $DEB_ARCH \
+  fpm --verbose -s dir -t deb --name $NAME --version $VER --license MIT --architecture $DEB_ARCH \
     --provides $NAME --description $NAME --url "https://github.com/mmmorris1975/$NAME" --maintainer 'mmmorris1975@github' \
     --deb-user bin --deb-group bin --prefix /usr/local/bin *
   mv *.deb ../artifacts/
