9c0d006a41811bbed4751c2a040d7a845e3bc24a
Mike Morris <michael.m.morris@gmail.com>
2020-12-18 11:26:56 -0600

    add deb and rpm packaging for linux builds

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,6 +1,8 @@
 set -e -x
 set -o pipefail
 
+NAME="x32-osc-bridge"
+
 function build() {
   VER=$(git describe --tags)
   BUILDDIR=${1:-"."}/${GOOS}-${GOARCH}
@@ -14,7 +16,41 @@ function pkg_zip() {
   cd $1
   upx *
   chmod +x *
-  zip x32-osc-bridge_$(cat ../version)_$(basename $PWD | tr '-' '_').zip *
+  zip ${NAME}_$(cat ../version)_$(basename $PWD | tr '-' '_').zip *
   mv *.zip ..
+  cd ..
+}
+
+function pkg_rpm() {
+  RPM_ARCH="x86_64"
+  if [[ `echo $d | grep -sc arm64$` > 0 ]]
+  then
+    RPM_ARCH="aarch64"
+  elif [[ `echo $d | grep -sc arm$` > 0 ]]
+  then
+    RPM_ARCH="armv7hl"
+  fi
+
+  cd $1
+  fpm --verbose -s dir -t rpm --name $NAME --version $(cat ../version) --license MIT --architecture $RPM_ARCH \
+    --provides $NAME --description $NAME --url "https://github.com/mmmorris1975/$NAME" --maintainer 'mmmorris1975@github' \
+    --rpm-user bin --rpm-group bin --rpm-digest sha1 --prefix /usr/local/bin
+  cd ..
+}
+
+function pkg_deb() {
+  DEB_ARCH="amd64"
+  if [[ `echo $d | grep -sc arm64$` > 0 ]]
+  then
+    DEB_ARCH="arm64"
+  elif [[ `echo $d | grep -sc arm$` > 0 ]]
+  then
+    DEB_ARCH="armhf"
+  fi
+
+  cd $1
+  fpm --verbose -s dir -t deb --name $NAME --version $(cat ../version) --license MIT --architecture $DEB_ARCH \
+    --provides $NAME --description $NAME --url "https://github.com/mmmorris1975/$NAME" --maintainer 'mmmorris1975@github' \
+    --deb-user bin --deb-group bin --prefix /usr/local/bin
   cd ..
 }
--- scripts/package_linux.sh
+++ scripts/package_linux.sh
@@ -8,5 +8,6 @@ for d in linux*
 do
   # create zip archives (opposed to tar archives, for consistency with macos and windows packaging)
   pkg_zip $d
-  # TODO use fpm to create DEB and RPM packages
+  pkg_deb $d
+  pkg_rpm $d
 done
