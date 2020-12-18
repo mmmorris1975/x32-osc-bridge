b7f3b9798c2a1efd2ab1371a9a40cd97415969ba
Mike Morris <michael.m.morris@gmail.com>
2020-12-18 16:03:37 -0600

    rework pipeline conditional release logic

---
+++ scripts/release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source scripts/helpers.sh
+
+# Only release if the commit is associated with a tag (maybe only matches a semver-like tag?)
+git describe --tags --exact-match --first-parent || exit 0
+
+sha256sum * >MANIFEST
+
+# TODO - actually release something
+cat MANIFEST
